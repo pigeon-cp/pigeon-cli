5a2d01e0715f502c34220fd23c11769464f10541
taccisum <liaojinfeng@deepexi.com>
2022-02-16 18:40:33 +0800

    docs: add simple user guide

---
+++ README.md
@@ -0,0 +1,39 @@
+# Pigeon CLI
+
+[Incubating]
+
+A CLI tools for project [Pigeon](https://github.com/pigeon-cp/pigeon).
+
+## Download
+
+if you have node.js installed in you machine, you can install `pigeon-cli` via npm by use the command below
+
+```bash
+$ npm install -g pigeon-cli
+```
+
+otherwise download the executable binary file from releases.
+
+
+## Getting Start
+
+### Plugin Debug
+
+```bash
+$ pigeon-cli debug
+```
+
+### Manage Pigeon Instance
+
+```bash
+$ pigeon-cli
+> pigeon.template.list()
+```
+
+## References
+
+```bash
+$ pigeon-cli --help
+```
+
+run command above for more informations.
--- cli.js
+++ cli.js
@@ -8,13 +8,31 @@ let program = new commander.Command();
 
 program.version(require('./package.json').version)
 
-program.usage('command(debug|admin|...) [options]')
+program.usage('[command] [options]')
+
+program.description('在交互模式下管理你的应用实例')
+.action((tmp, port) => {
+    let server = repl.start('> ')
+
+    server.context.pigeon = {
+        template: {
+            list() {
+                console.log('template1, template2, template3')
+            },
+            add () {
+                console.log('新增模板成功')
+            }
+        }
+    }
+});
 
 program.command('debug')
 .addArgument(new commander.Argument('[port]', 'JDWP debug 连接端口').default('56789'))
 .option('-p --path <value>', 'Pigeon 可执行 jar 所在路径')
 .description('以 Debug 模式启动 Pigeon 应用实例')
 .action((port, opts) => {
+    console.log(`Java Debugger 连接参数: -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${port}`)
+
     var result = spawn('java', [
         `-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=${port}`,
         '-jar', `${opts.path || '/Users/liaojinfeng/Documents/workspace/tac/code/java/pegion/target'}/pigeon.jar`,
@@ -32,30 +50,8 @@ program.command('debug')
     });
 });
 
-program.command('admin')
-.description('在交互模式下管理你的应用实例')
-.action((tmp, port) => {
-    console.log('start: ', tmp)
-    console.log(`jdwp=transport=dt_socket,server=y,address=${port}`)
-    let server = repl.start('> ')
-
-
-    server.context.say = function(msg) {
-        console.log(msg)
-        return 'success'
-    }
-});
-
-program.command('say')
-.argument('<what...>')
-.action(what => {
-    console.log(`you say: ${what}`)
-})
 
 program.parse();
-
-console.log('Options: ', program.opts());
-console.log('Remaining arguments: ', program.args);
 
 
 
